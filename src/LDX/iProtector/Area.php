1b5aee370b666f692071d04ef789cfbc806d4559
Luke Darling <HTMLguy7@gmail.com>
2014-07-27 13:40:09 -0400

    Update Area.php

--- src/LDX/iProtector/Area.php
+++ src/LDX/iProtector/Area.php
@@ -1,19 +1,34 @@
 <?php
 namespace LDX\iProtector;
+use pocketmine\Player;
+use pocketmine\math\Vector3;
 class Area {
-  public function __construct($name,$flags,$pos1,$pos2,$plugin) {
-    $this->name = $name;
-    $this->flags = $flags;
-    $this->pos1 = $pos1;
-    $this->pos2 = $pos2;
-    $this->plugin = $plugin;
+  public function __construct($data) {
+    $this->name = $data["name"];
+    $this->flags = $data["flags"];
+    $this->pos1 = $data["pos1"];
+    $this->pos2 = $data["pos2"];
+    $this->plugin = $data["plugin"];
+  }
+  public function getName() {
+    return $this->name;
+  }
+  public function getFlags() {
+    return $this->flags;
+  }
+  public function getFlag($flag) {
+    return $this->flags[$flag];
   }
   public function setFlag($flag,$value) {
     $this->flags[$flag] = $value;
+    return $value;
   }
   public function toggleFlag($flag) {
     $this->flags[$flag] = !$this->flags[$flag];
     return $this->flags[$flag];
+  }
+  public function getPos() {
+    return array($this->pos1,$this->pos2);
   }
   public function __destruct() {
     $this->plugin->saveArea($this);
