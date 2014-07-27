aed2f57bb95dfa87564e025d7e8eb32a200ca317
Luke Darling <HTMLguy7@gmail.com>
2014-07-26 22:24:20 -0400

    Update Area.php

--- src/LDX/iProtector/Area.php
+++ src/LDX/iProtector/Area.php
@@ -1,1 +1,22 @@
-null
+<?php
+namespace LDX\iProtector;
+class Area {
+  public function __construct($name,$flags,$pos1,$pos2,$plugin) {
+    $this->name = $name;
+    $this->flags = $flags;
+    $this->pos1 = $pos1;
+    $this->pos2 = $pos2;
+    $this->plugin = $plugin;
+  }
+  public function setFlag($flag,$value) {
+    $this->flags[$flag] = $value;
+  }
+  public function toggleFlag($flag) {
+    $this->flags[$flag] = !$this->flags[$flag];
+    return $this->flags[$flag];
+  }
+  public function __destruct() {
+    $this->plugin->saveArea($this);
+  }
+}
+?>
