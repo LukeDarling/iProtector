f1e3b63241b04bfb4eb535a310cf4da89d7c2d71
Falkirks <Falkirks@users.noreply.github.com>
2014-07-29 14:37:53 -0700

    Update Main.php

--- src/LDX/iProtector/Main.php
+++ src/LDX/iProtector/Main.php
@@ -1,18 +1,17 @@
 <?php
 namespace LDX\iProtector;
-use LDX\iProtector\Area;
 use pocketmine\math\Vector3;
 use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\block\BlockBreakEvent;
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements CommandExecutor, Listener {
   public function onEnable() {
     $this->getServer()->getPluginManager()->registerEvents($this,$this);
     @mkdir($this->getDataFolder());
