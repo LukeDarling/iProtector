65c7bf074b5d151c6161b95ce1b2bd2d22929804
LDX <htmlguy7@yahoo.com>
2014-12-25 19:56:54 -0500

    Update Area.php

--- src/LDX/iProtector/Area.php
+++ src/LDX/iProtector/Area.php
@@ -1,55 +1,93 @@
 <?php
+
 namespace LDX\iProtector;
+
 use pocketmine\math\Vector3;
+
 class Area {
-  public function __construct($data,$plugin) {
-    $this->name = strtolower($data["name"]);
-    $this->flags = $data["flags"];
-    $this->pos1 = new Vector3($data["pos1"][0],$data["pos1"][1],$data["pos1"][2]);
-    $this->pos2 = new Vector3($data["pos2"][0],$data["pos2"][1],$data["pos2"][2]);
+
+  private $name;
+  private $flags;
+  private $pos1;
+  private $pos2;
+  private $level;
+  private $plugin;
+
+  public function __construct($name,$flags,$pos1,$pos2,$level,$plugin) {
+    $this->name = strtolower($name);
+    $this->flags = $flags;
+    $this->pos1 = new Vector3($pos1[0],$pos1[1],$pos1[2]);
+    $this->pos2 = new Vector3($pos2[0],$pos2[1],$pos2[2]);
+    $this->level = strtolower($level);
     $this->plugin = $plugin;
     $this->save();
   }
+
   public function getName() {
     return $this->name;
   }
+
   public function getFlags() {
     return $this->flags;
   }
+
   public function getFlag($flag) {
-    return $this->flags[$flag];
-  }
-  public function setFlag($flag,$value) {
-    $this->flags[$flag] = $value;
-    $this->save();
-    $this->plugin->saveAreas();
-    return $value;
-  }
-  public function contains($ppos) {
-    if((min($this->pos1->getX(),$this->pos2->getX()) <= $ppos->getX()) && (max($this->pos1->getX(),$this->pos2->getX()) >= $ppos->getX()) && (min($this->pos1->getY(),$this->pos2->getY()) <= $ppos->getY()) && (max($this->pos1->getY(),$this->pos2->getY()) >= $ppos->getY()) && (min($this->pos1->getZ(),$this->pos2->getZ()) <= $ppos->getZ()) && (max($this->pos1->getZ(),$this->pos2->getZ()) >= $ppos->getZ())) {
-      return true;
-    } else {
-      return false;
+    if(isset($this->flags[$flag])) {
+      return $this->flags[$flag];
     }
+    return false;
   }
+
+  public function setFlag($flag,$value) {
+    if(isset($this->flags[$flag])) {
+      $this->flags[$flag] = $value;
+      $this->save();
+      $this->plugin->saveAreas();
+      return true;
+    }
+    return false;
+  }
+
+  public function getPos1() {
+    return $this->pos1;
+  }
+
+  public function getPos2() {
+    return $this->pos2;
+  }
+
+  public function getLevel() {
+    return $this->level;
+  }
+
+  public function contains($pos,$level) {
+    if((min($this->pos1->getX(),$this->pos2->getX()) <= $pos->getX()) && (max($this->pos1->getX(),$this->pos2->getX()) >= $pos->getX()) && (min($this->pos1->getY(),$this->pos2->getY()) <= $pos->getY()) && (max($this->pos1->getY(),$this->pos2->getY()) >= $pos->getY()) && (min($this->pos1->getZ(),$this->pos2->getZ()) <= $pos->getZ()) && (max($this->pos1->getZ(),$this->pos2->getZ()) >= $pos->getZ()) && ($this->level == $level)) {
+      return true;
+    }
+    return false;
+  }
+
   public function toggleFlag($flag) {
-    $this->flags[$flag] = !$this->flags[$flag];
-    $this->save();
-    $this->plugin->saveAreas();
-    return $this->flags[$flag];
+    if(isset($this->flags[$flag])) {
+      $this->flags[$flag] = !$this->flags[$flag];
+      $this->save();
+      $this->plugin->saveAreas();
+      return $this->flags[$flag];
+    }
+    return false;
   }
-  public function getData() {
-    return array("name" => $this->name,"flags" => $this->flags,"pos1" => array($this->pos1->getX(),$this->pos1->getY(),$this->pos1->getZ()),"pos2" => array($this->pos2->getX(),$this->pos2->getY(),$this->pos2->getZ()));
-  }
+
   public function save() {
     $this->plugin->areas[$this->name] = $this;
-    $this->plugin->areadata[$this->name] = $this->getData();
+    return true;
   }
+
   public function delete() {
     $name = $this->getName();
     unset($this->plugin->areas[$name]);
-    unset($this->plugin->areadata[$name]);
     $this->plugin->saveAreas();
+    return true;
   }
+
 }
 ?>
