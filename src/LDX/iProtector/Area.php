560d89b6ba2768deab9fe9057346c597c247c32b
LDX <htmlguy7@yahoo.com>
2015-02-14 21:01:15 -0500

    Update Area.php

--- src/LDX/iProtector/Area.php
+++ src/LDX/iProtector/Area.php
@@ -7,24 +7,34 @@ use pocketmine\math\Vector3;
 class Area {
 
   private $name;
-  private $flags;
+  public $flags;
   private $pos1;
   private $pos2;
   private $level;
+  private $whitelist;
   private $plugin;
 
-  public function __construct($name,$flags,$pos1,$pos2,$level,$plugin) {
+  public function __construct($name,$flags,$pos1,$pos2,$level,$whitelist,$plugin) {
     $this->name = strtolower($name);
     $this->flags = $flags;
     $this->pos1 = new Vector3($pos1[0],$pos1[1],$pos1[2]);
     $this->pos2 = new Vector3($pos2[0],$pos2[1],$pos2[2]);
-    $this->level = strtolower($level);
+    $this->level = $level;
+    $this->whitelist = $whitelist;
     $this->plugin = $plugin;
     $this->save();
   }
 
   public function getName() {
     return $this->name;
+  }
+
+  public function getPos1() {
+    return array($this->pos1->getX(),$this->pos1->getY(),$this->pos1->getZ());
+  }
+
+  public function getPos2() {
+    return array($this->pos2->getX(),$this->pos2->getY(),$this->pos2->getZ());
   }
 
   public function getFlags() {
@@ -41,23 +51,10 @@ class Area {
   public function setFlag($flag,$value) {
     if(isset($this->flags[$flag])) {
       $this->flags[$flag] = $value;
-      $this->save();
       $this->plugin->saveAreas();
       return true;
     }
     return false;
-  }
-
-  public function getPos1() {
-    return $this->pos1;
-  }
-
-  public function getPos2() {
-    return $this->pos2;
-  }
-
-  public function getLevel() {
-    return $this->level;
   }
 
   public function contains($pos,$level) {
@@ -70,11 +67,43 @@ class Area {
   public function toggleFlag($flag) {
     if(isset($this->flags[$flag])) {
       $this->flags[$flag] = !$this->flags[$flag];
-      $this->save();
       $this->plugin->saveAreas();
       return $this->flags[$flag];
     }
     return false;
+  }
+
+  public function getLevel() {
+    return $this->level;
+  }
+
+  public function isWhitelisted($n) {
+    if(in_array($n,$this->whitelist)) {
+      return true;
+    }
+    return false;
+  }
+
+  public function setWhitelisted($n,$v = true) {
+    if($v) {
+      if(!in_array($n,$this->whitelist)) {
+        array_push($this->whitelist,$n);
+        $this->plugin->saveAreas();
+        return true;
+      }
+    } else {
+      if(in_array($n,$this->whitelist)) {
+        $key = array_search($n,$this->whitelist);
+        array_splice($this->whitelist,$key,1);
+        $this->plugin->saveAreas();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public function getWhitelist() {
+    return $this->whitelist;
   }
 
   public function save() {
@@ -83,8 +112,7 @@ class Area {
   }
 
   public function delete() {
-    $name = $this->getName();
-    unset($this->plugin->areas[$name]);
+    unset($this->plugin->areas[$this->getName()]);
     $this->plugin->saveAreas();
     return true;
   }
